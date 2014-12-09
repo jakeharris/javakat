eff1c26c4cde10095447007de455245a99702bca
Jake H <milieu@javakat.io>
2014-12-09 15:07:09 -0600

    Change app name to 'javakat' in config/environment.js

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'app',
+    modulePrefix: 'javakat',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
