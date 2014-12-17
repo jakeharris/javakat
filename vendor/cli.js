593f18d0f9c228d3fd48ba82e8194ba22084cec2
Jake <milieu@javakat.io>
2014-12-17 10:16:45 -0600

    Make cli.js run; give it basic things to do

--- vendor/cli.js
+++ vendor/cli.js
@@ -8,44 +8,91 @@
     return false
   }
   
-  // # Grab the input element.
-  var input = document.getElementsByTagName('input')[0],
-      output = document.getElementById('content'),
-      commands = [
-        'man',
-        'help',
-        'ouroboros'
-      ]
+  document.addEventListener("DOMContentLoaded", function(event) {
   
-  
-  // # On `enter` keyup:
-  input.onkeyup = function (e) {
-    if (e.keyCode !== 13) return;
+    // # Grab the input element.
+    var input = document.getElementsByTagName('input')[0],
+        output = document.getElementById('content'),
+        commands = [
+          'man',
+          'help',
+          'ouroboros'
+        ]
+
+    output.appendLine = function (line) {
+      var d = document.createElement('p')
+      d.innerHTML = line
+      this.appendChild(d)
+    }
     
-    // ## Shift the page so the CLI is at the top and the output is 90% of the height
-    
-    
-    // ## Attempt to run the given command
-    // ### Break the command down into command and args
-    var command = input.value.split(' ')[0],
-        args = input.value.substr(command.length).split(' ')
-    
-    // ### Determine if command is valid
-    // TODO: Investigate -- could this be automatable? Think Seth's bot.
-    // If this code could be 'aware' of all the possible runnable commands
-    // without needing to manually enumerate them in the commands array...
-    console.log('command: ' + command)
-    
-    if(commands.contains(command))
-      run(command)
+    input.focus()
+
+    // # On `enter` keyup:
+    input.onkeyup = function (e) {
+      if (e.keyCode !== 13) return;
+
+      // ## Shift the page so the CLI is at the top and the output is 90% of the height
       
-    else
-      output.appendChild(document.createElement('p').innerHTML('unknown command: ' + command + '.\nType \'help\' for usage.'))
+
+      // ## Attempt to run the given command
+      // ### Break the command down into command and args
+      var command = input.value.split(' ')[0],
+          args = input.value.substr(command.length + 1).split(' ')
+
+      // ### Determine if command is valid
+      // TODO: Investigate -- could this be automatable? Think Seth's bot.
+      // If this code could be 'aware' of all the possible runnable commands
+      // without needing to manually enumerate them in the commands array...
+      output.appendLine('> ' + input.value)
+      input.value = ''
+      
+      if(commands.contains(command))
+        run(command, args)
+
+      else {
+        output.appendLine('unknown command: ' + command + '.\nType \'help\' for usage.')
+      }
+    }
+
+    var run = function (command, args) {
+      
+      switch(command) {
+        case 'man':
+          man(args)
+          break
+        case 'help':
+          help(args)
+          break
+        case 'ouroboros':
+          ouroboros(args)
+          break
+        default:
+          output.appendLine('Couldn\'t run command `' + command + '`.\nType \'help\' for a list of available commands.')
+          break
+      }
+              
+      output.appendLine('&nbsp;')
+        
+    }
     
-  }
-  
-  var run = function (command) {
-     
-  }
+    var man = function (args) {
+      output.appendLine('man ' + args + '...')
+      
+      if(args[0] === 'javakat')
+        help(args)
+      else
+        output.appendLine('(man functionality is currently unwritten.)')
+    }
+    
+    var help = function (args) {
+      output.appendLine('===== COMMAND LIST =====')
+      output.appendLine('man&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;launches a manual page about the topic')
+      output.appendLine('ouroboros&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;launches a game of Ouroboros on the page')
+      output.appendLine('&lt;member name, handle&gt;&nbsp;&nbsp;&nbsp;&nbsp;brings you to that member\'s personal page')
+      output.appendLine('&nbsp;')
+      output.appendLine('and `help`, which will print this prompt again.')
+    }
+    
+  })
   
 })()
